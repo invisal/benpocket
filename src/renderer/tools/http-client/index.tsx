95409ee8b590717e9c2ca662a525123a1f13beab
Visal In <invisal@gmail.com>
2026-08-22 13:50:02 +0100

    feat: refactor HttpClientMain layout and restore new request button

--- src/renderer/tools/http-client/index.tsx
+++ src/renderer/tools/http-client/index.tsx
@@ -1,14 +1,33 @@
 import { type ToolComponentProps } from '@renderer/components/providers/createTabProvider';
 import { ToolLayout } from '@renderer/components/layout/ToolLayout';
+import { Button } from '@renderer/components/ui/Button';
+import { Plus } from 'lucide-react';
 import { HttpClientWorkspace } from './HttpClientWorkspace';
+import { WorkspaceSelector } from './components/WorkspaceSelector';
+import { useRequestTabsStore } from './store/tabs.store';
 
 interface Props {}
 
 // eslint-disable-next-line no-empty-pattern
 export function HttpClientMain({}: ToolComponentProps<Props>) {
+  const openNewRequestTab = useRequestTabsStore((s) => s.openNewRequestTab);
+
   return (
     <>
-      <ToolLayout.Title>HTTP Client</ToolLayout.Title>
+      <ToolLayout.Title>
+        <div className="titlebar-nodrag inline-flex items-center gap-2">
+          <WorkspaceSelector />
+          <Button
+            variant="ghost"
+            size="sm"
+            className="h-6 px-1.5"
+            onClick={() => openNewRequestTab({ method: 'GET', url: '' })}
+            title="Create Request"
+          >
+            <Plus size={14} />
+          </Button>
+        </div>
+      </ToolLayout.Title>
       <HttpClientWorkspace />
     </>
   );
